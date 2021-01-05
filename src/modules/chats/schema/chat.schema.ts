b005c527444f1baef109dea7506d66a69d3e7210
gabriel <cristhian.viera1@gmail.com>
2021-01-05 12:39:11 -0500

    add serverless

--- src/modules/chats/schema/chat.schema.ts
+++ src/modules/chats/schema/chat.schema.ts
@@ -1,7 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 import { Document } from 'mongoose';
 import { v4 as uuid4 } from 'uuid';
 import { generateUnixTimestamp } from '../../../utils/generateUnixTimestamp';
+
 
 export interface Message {
   type: 'text' | 'image';
@@ -32,7 +34,7 @@ export class Chat extends Document {
   @Prop({ required: true })
   toUser: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, type: mongoose.Schema.Types.Mixed })
   message: Message;
 
   @Prop({
